fecba8acd0ce99cd0af1c6c5dd10d03a3715b8ec
Kash <andy15158515@gmail.com>
2026-06-07 12:35:56 -0700

    feat: create task and ready for commit

--- Blinky_Demo/main_blinky.c
+++ Blinky_Demo/main_blinky.c
@@ -37,55 +37,27 @@
  * required to configure the hardware are defined in main.c.
  ******************************************************************************
  *
- * main_blinky() creates one queue, and two tasks.  It then starts the
- * scheduler.
- *
- * The Queue Send Task:
- * The queue send task is implemented by the prvQueueSendTask() function in
- * this file.  prvQueueSendTask() sits in a loop that causes it to repeatedly
- * block for 200 milliseconds, before sending the value 100 to the queue that
- * was created within main_blinky().  Once the value is sent, the task loops
- * back around to block for another 200 milliseconds...and so on.
- *
- * The Queue Receive Task:
- * The queue receive task is implemented by the prvQueueReceiveTask() function
- * in this file.  prvQueueReceiveTask() sits in a loop where it repeatedly
- * blocks on attempts to read data from the queue that was created within
- * main_blinky().  When data is received, the task checks the value of the
- * data, and if the value equals the expected 100, toggles an LED.  The 'block
- * time' parameter passed to the queue receive function specifies that the
- * task should be held in the Blocked state indefinitely to wait for data to
- * be available on the queue.  The queue receive task will only leave the
- * Blocked state when the queue send task writes to the queue.  As the queue
- * send task writes to the queue every 200 milliseconds, the queue receive
- * task leaves the Blocked state every 200 milliseconds, and therefore toggles
- * the LED every 200 milliseconds.
+ * For Lab 5 this file runs a single checkpoint test task.  The task commits a
+ * full checkpoint every ten iterations, occasionally enters LPM4.5 to simulate
+ * power failure, and then continues from the last committed CPU context after
+ * reset or power restoration.
  */
 
 /* Kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
+#include "checkpoint.h"
 
 /* Standard demo includes. */
 #include "partest.h"
+#include <stdio.h>
 
 /* Priorities at which the tasks are created. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
-#define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
+#define mainCHECKPOINT_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
 
-/* The rate at which data is sent to the queue.  The 200ms value is converted
-to ticks using the portTICK_PERIOD_MS constant. */
-#define mainQUEUE_SEND_FREQUENCY_MS			( pdMS_TO_TICKS( 200 ) )
-
-/* The number of items the queue can hold.  This is 1 as the receive task
-will remove items as they are added, meaning the send task should always find
-the queue empty. */
-#define mainQUEUE_LENGTH					( 1 )
-
-/* The LED toggled by the Rx task. */
-#define mainTASK_LED						( 0 )
-
+#define mainCHECKPOINT_DELAY	 	( pdMS_TO_TICKS( 200 ) )
+#define mainCHECKPOINT_PERIOD		( 10UL )
 /*-----------------------------------------------------------*/
 
 /*
@@ -97,37 +69,31 @@ void main_lab( void );
 /*
  * The tasks as described in the comments at the top of this file.
  */
-static void prvQueueReceiveTask( void *pvParameters );
-static void prvQueueSendTask( void *pvParameters );
+static void prvCheckPointTask( void *pvParameters );
+static void prvRandom( void *pvParameters );
+static void prvPowerOff( void *pvParameters );
+static void prvCommit( void *pvParameters );
 
 /*-----------------------------------------------------------*/
 
-/* The queue used by both tasks. */
-static QueueHandle_t xQueue = NULL;
+/* variables */
+static volatile uint32_t ulCheckpointIterations;
 
 /*-----------------------------------------------------------*/
 
 void main_lab( void )
 {
-	/* Create the queue. */
-	xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( uint32_t ) );
+	/* Start the task as described in the comments at the top of this file. */
+	xTaskCreate( prvCheckpointTask,				/* The function that implements the task. */
+				"CHK", 								/* The text name assigned to the task - for debug only as it is not used by the kernel. */
+				configMINIMAL_STACK_SIZE, 			/* The size of the stack to allocate to the task. */
+				NULL, 								/* The parameter passed to the task - not used in this case. */
+				mainCHECKPOINT_TASK_PRIORITY,    	/* The priority assigned to the task. */
+				NULL );								/* The task handle is not required, so NULL is passed. */
 
-	if( xQueue != NULL )
-	{
-		/* Start the two tasks as described in the comments at the top of this
-		file. */
-		xTaskCreate( prvQueueReceiveTask,				/* The function that implements the task. */
-					"Rx", 								/* The text name assigned to the task - for debug only as it is not used by the kernel. */
-					configMINIMAL_STACK_SIZE, 			/* The size of the stack to allocate to the task. */
-					NULL, 								/* The parameter passed to the task - not used in this case. */
-					mainQUEUE_RECEIVE_TASK_PRIORITY, 	/* The priority assigned to the task. */
-					NULL );								/* The task handle is not required, so NULL is passed. */
+	/* Start the tasks and timer running. */
+	vTaskStartScheduler();
 
-		xTaskCreate( prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK_PRIORITY, NULL );
-
-		/* Start the tasks and timer running. */
-		vTaskStartScheduler();
-	}
 
 	/* If all is well, the scheduler will now be running, and the following
 	line will never be reached.  If the following line does execute, then
@@ -139,7 +105,7 @@ void main_lab( void )
 }
 /*-----------------------------------------------------------*/
 
-static void prvQueueSendTask( void *pvParameters )
+static void prvCheckpointTask( void *pvParameters )
 {
 TickType_t xNextWakeTime;
 const unsigned long ulValueToSend = 100UL;
@@ -150,43 +116,30 @@ const unsigned long ulValueToSend = 100UL;
 	/* Initialise xNextWakeTime - this only needs to be done once. */
 	xNextWakeTime = xTaskGetTickCount();
 
-	for( ;; )
+	while(ulCheckpointIterations++;)
 	{
 		/* Place this task in the blocked state until it is time to run again. */
-		vTaskDelayUntil( &xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS );
+		vTaskDelayUntil( &xNextWakeTime,  mainCHECKPOINT_DELAY );
 
-		/* Send to the queue - causing the queue receive task to unblock and
-		toggle the LED.  0 is used as the block time so the sending operation
-		will not block - it shouldn't need to block as the queue should always
-		be empty at this point in the code. */
-		xQueueSend( xQueue, &ulValueToSend, 0U );
+		if(prvRandom()) prvPowerOff(); // Todo: enter LPM4.5
+		if(ulCheckpointIterations % mainCHECKPOINT_PERIOD == 0) prvCommit();
+		printf("%lu\n", i);
 	}
 }
 /*-----------------------------------------------------------*/
 
-static void prvQueueReceiveTask( void *pvParameters )
+static BaseType_t prvRandom( void *pvParameters)
 {
-unsigned long ulReceivedValue;
-const unsigned long ulExpectedValue = 100UL;
+	( void ) pvParameters;
+	if(ulCheckpointIterations % 24 == 0) return pdTRUE;
+	return pdFALSE;
+}
 
-	/* Remove compiler warning about unused parameter. */
+/*-----------------------------------------------------------*/
+
+static void prvCommit( void *pvParameters)
+{
 	( void ) pvParameters;
 
-	for( ;; )
-	{
-		/* Wait until something arrives in the queue - this task will block
-		indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
-		FreeRTOSConfig.h. */
-		xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
-
-		/*  To get here something must have been received from the queue, but
-		is it the expected value?  If it is, toggle the LED. */
-		if( ulReceivedValue == ulExpectedValue )
-		{
-			vParTestToggleLED( mainTASK_LED );
-			ulReceivedValue = 0U;
-		}
-	}
+	
 }
-/*-----------------------------------------------------------*/
-
